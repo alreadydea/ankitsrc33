b5ded5e7dc29a9a3e5657547fc1ae6fe012a5312
Gagan <119489004+devgaganin@users.noreply.github.com>
2023-12-15 01:33:05 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -46,28 +46,11 @@ async def remt(event):
 async def start(event):
     text = "👋 Hi, I am 'Save Restricted Content Bot' Made with ❤️ __Gagan__\n\n✅ Send me the Link of any message of Restricted Channels to Clone it here..."
 
-    photo_filename = 'photo.jpg'  # Replace with the actual filename of the photo
-
-    try:
-        with open(photo_filename, 'rb') as photo_file:
-            photo_data = BytesIO(photo_file.read())
-
-        await event.client.send_file(
-            event.chat_id,
-            file=photo_data,
-            caption=text,
-            buttons=[
-                [Button.inline("SET THUMB.", data="set"),
-                 Button.inline("REM THUMB.", data="rem")],
-                Button.url("Join Channel", url="https://t.me/dev_gagan")
-            ]
-        )
-    except FileNotFoundError:
-        print(f"File not found: {photo_filename}")
-    except Exception as e:
-        print(f"Error: {e}")
-
-
-
+    await event.reply(text, 
+                      buttons=[
+                              [Button.inline("SET THUMB.", data="set"),
+                               Button.inline("REM THUMB.", data="rem")],
+                              [Button.url("Join Channel", url="https://telegram.dog/dev_gagan")]])
+    
     
     
