76cad5f287a3f8051f657d96648aedfad1aa559f
Gagan <119489004+devgaganin@users.noreply.github.com>
2023-12-14 22:41:28 +0530

    Update batch.py

--- main/plugins/batch.py
+++ main/plugins/batch.py
@@ -36,20 +36,20 @@ ids = []
     msg = await userbot.get_messages(chat, ids=id)
     await event.client.send_message(event.chat_id, msg) 
 '''   
-@Invix.on(events.NewMessage(incoming=True, from_users=SUDO_USERS, pattern='/batch'))
+@Invix.on(events.NewMessage(incoming=True, pattern='/batch'))
 async def _batch(event):
     '''
     #if not event.is_private:
     #    return
     # wtf is the use of fsub here if the command is meant for the owner? 
     # well am too lazy to clean 
-    #s, r = await force_sub(event.client, fs, event.sender_id, ft) 
+    #s, r = await force_sub(event.client, fs, event.chat_id, ft) 
     #if s == True:
     #   await event.reply(r)
     #  return       
     '''
     s = False
-    if f'{event.sender_id}' in batch:
+    if f'{event.chat_id}' in batch:
         return await event.reply("You've already started one batch, wait for it to complete you dumbfuck owner!")
     async with Invix.conversation(event.chat_id) as conv: 
         if not s:
@@ -83,16 +83,16 @@ async def _batch(event):
             if s != True:
                 await conv.send_message(r)
                 return
-            batch.append(f'{event.sender_id}')
+            batch.append(f'{event.chat_id}')
             cd = await conv.send_message("**Batch process ongoing.**\n\nProcess completed: ", 
                                     buttons=[[Button.inline("CANCEL❌", data="cancel")]])
-            co = await run_batch(userbot, Bot, event.sender_id, cd, _link) 
+            co = await run_batch(userbot, Bot, event.chat_id, cd, _link) 
             try: 
                 if co == -2:
-                    await Bot.send_message(event.sender_id, "Batch successfully completed!")
+                    await Bot.send_message(event.chat_id, "Batch successfully completed!")
                     await cd.edit(f"**Batch process ongoing.**\n\nProcess completed: {value} \n\n Batch successfully completed! ")
             except:
-                await Bot.send_message(event.sender_id, "ERROR!\n\n maybe last msg didnt exist yet")
+                await Bot.send_message(event.chat_id, "ERROR!\n\n maybe last msg didnt exist yet")
             conv.cancel()
             ids.clear()
             batch.clear()
@@ -101,7 +101,7 @@ async def _batch(event):
 async def cancel(event):
     ids.clear()
     
-async def run_batch(userbot, client, sender, countdown, link):
+async def run_batch(userbot, client, chat_id, countdown, link):
     for i in range(len(ids)):
         timer = 6
         if i < 250:
@@ -128,32 +128,32 @@ async def run_batch(userbot, client, sender, countdown, link):
                 msg_id = int(link.split("/")[-1])
             except ValueError:
                 if '?single' not in link:
-                    return await client.send_message(sender, "**Invalid Link! .**")
+                    return await client.send_message(chat_id, "**Invalid Link! .**")
                 link_ = link.split("?single")[0]
                 msg_id = int(link_.split("/")[-1])
             integer = msg_id + int(ids[i])
-            await get_bulk_msg(userbot, client, sender, link, integer)
-            protection = await client.send_message(sender, f"Sleeping for `{timer}` seconds to avoid Floodwaits and Protect account!")
+            await get_bulk_msg(userbot, client, chat_id, link, integer)
+            protection = await client.send_message(chat_id, f"Sleeping for `{timer}` seconds to avoid Floodwaits and Protect account!")
             await countdown.edit(count_down, 
                                  buttons=[[Button.inline("CANCEL❌", data="cancel")]])
             await asyncio.sleep(timer)
             await protection.delete()
         except IndexError as ie:
-            await client.send_message(sender, f" {i}  {ie}  \n\nBatch ended completed!")
+            await client.send_message(chat_id, f" {i}  {ie}  \n\nBatch ended completed!")
             await countdown.delete()
             break
         except FloodWait as fw:
             if int(fw.value) > 300:
-                await client.send_message(sender, f'You have floodwaits of {fw.value} seconds, cancelling batch') 
+                await client.send_message(chat_id, f'You have floodwaits of {fw.value} seconds, cancelling batch') 
                 ids.clear()
                 break
             else:
-                fw_alert = await client.send_message(sender, f'Sleeping for {fw.value + 5} second(s) due to telegram flooodwait.')
+                fw_alert = await client.send_message(chat_id, f'Sleeping for {fw.value + 5} second(s) due to telegram flooodwait.')
                 ors = fw.value + 5
                 await asyncio.sleep(ors)
                 await fw_alert.delete()
                 try:
-                    await get_bulk_msg(userbot, client, sender, link, integer)
+                    await get_bulk_msg(userbot, client, chat_id, link, integer)
                 except Exception as e:
                     #print(e)
                     logger.info(e)
@@ -162,7 +162,7 @@ async def run_batch(userbot, client, sender, countdown, link):
         except Exception as e:
             #print(e)
             logger.info(e)
-            await client.send_message(sender, f"An error occurred during cloning, batch will continue.\n\n**Error:** {str(e)}")
+            await client.send_message(chat_id, f"An error occurred during cloning, batch will continue.\n\n**Error:** {str(e)}")
             if countdown.text != count_down:
                 await countdown.edit(count_down, buttons=[[Button.inline("CANCEL❌", data="cancel")]])
         n = i + 1
