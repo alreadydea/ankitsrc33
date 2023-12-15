5af5ae0405bf801357c6feab2ff819bbc071c91d
Gagan <119489004+devgaganin@users.noreply.github.com>
2023-12-15 13:58:37 +0530

    Update pyroplug.py

--- main/plugins/pyroplug.py
+++ main/plugins/pyroplug.py
@@ -133,7 +133,7 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i, file_n):
                 progress=progress_for_pyrogram,
                 progress_args=(
                     client,
-                    "**DOWNLOADING:**\n\n**🔥 __Team SPY__ 🔥**",
+                    "**DOWNLOADING: __Team SPY__**\n ",
                     edit,
                     time.time()
                 )
@@ -186,7 +186,7 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i, file_n):
                     progress=progress_for_pyrogram,
                     progress_args=(
                         client,
-                        '**UPLOADING:**\n\n**🔥 __Team SPY__ 🔥**',
+                        '**UPLOADING: __Team SPY__**\n ',
                         upm,
                         time.time()
                     )
