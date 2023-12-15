3fc303af6debde929746a0450d4484337ad180f4
Gagan <119489004+devgaganin@users.noreply.github.com>
2023-12-15 11:08:19 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -16,12 +16,12 @@ logging.getLogger("telethon").setLevel(logging.WARNING)
 
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None)
+API_ID = "28034111"
+API_HASH = "3f89fab357b70e6556aa6a9e1d892b9d"
+BOT_TOKEN = "6484894365:AAEagaHJ-zAP3i6SLlMNUleSBhGwKWduYOw"
+SESSION = "BABbqRS2rE4g3fJ4C1tncdjoWeOCDWdHHSIvWY7cIFqvQLR15tAreFakxUS6KYP8FcNGh0Zne7W0CyJPAv_qBm1wUGuJXpojRYEQ4UIY52lQpUaST6lPwdIX-4Nz8x9rcbpVV3hIIiTS3Ln5Up604S-19am1uHv4BuC9bBqpWUVB4M8wCLWlosxFoeAWDvVnX_3xzjYnh5FmEgMIAwNj_GoY34fCtI8BSwrbiOJq7M0NUrEEOKQ1KmvfJWsluxB6gg7gHzluJJCW_1dqVpHd-qGDcXZ8tbzRAniD7qhRxSQE6VGTRNF9W4GT_NlDixcjOwJ5RZ9LF0_RevSws8IrkC1tAAAAAYSLfM0A"
+FORCESUB = "dev_gagan"
+AUTH = "6876018655"
 SUDO_USERS = []
 if len(AUTH) != 0:
     SUDO_USERS = {int(AUTH.strip()) for AUTH in AUTH.split()}
