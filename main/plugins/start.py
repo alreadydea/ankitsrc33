208e7675bf1cfbbe2fc52f0746f1c15ae7f0d853
Gagan <119489004+devgaganin@users.noreply.github.com>
2023-12-15 11:58:43 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -5,7 +5,7 @@ from telethon.tl.types import InputMediaPhoto
 
 S = "/start"
 START_PIC = "https://graph.org/file/ffd7da274e555ed3a9fee.jpg"
-TEXT = "👋 Hi, I am 'Save Restricted Content' bot Made with ❤️ by __**Team SPY**__\n\n✅ Send me the Link of any message of Restricted Channels to Clone it here.\nFor private channel's messages, send the Invite Link first.\n\n👨🏻‍💻Owner: https://telegram.dog/Mister_invisiblebot.\n**support:** https://telegram.dog/mr_invisible_bots"
+TEXT = "👋 Hi, I am 'Save Restricted Content' bot Made with ❤️ by __**Team SPY**__\n\n✅ Send me the Link of any message of Restricted Channels to Clone it here.\nFor private channel's messages, send the Invite Link first.
 
 def is_set_button(data):
     return data == "set"
